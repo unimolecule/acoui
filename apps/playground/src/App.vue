43fd8e32c49a29ad03bd1c544fce84b8fb7c9fc4
YeSuX <351490949@qq.com>
2023-04-09 13:01:17 +0800

    feat(playground): :sparkles: add template

--- apps/playground/src/App.vue
+++ apps/playground/src/App.vue
@@ -4,6 +4,25 @@ import { AcoButton } from '@aco/ui'
 </script>
 
 <template>
-  <HButton />
-  <AcoButton />
+  <div class="container">
+    <div class="box">
+      <HButton />
+    </div>
+    <div class="box">
+      <AcoButton />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.container{
+  display: flex;
+  justify-content: space-around;
+}
+.box{
+  border: 1px dotted #888;
+  width: 40vw;
+  padding: 10px;
+  min-height: 300px;
+}
+</style>
