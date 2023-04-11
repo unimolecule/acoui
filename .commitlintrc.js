9f76e148ad798507eaeff9174d15942bd3b81982
YeSuX <351490949@qq.com>
2023-04-11 11:52:01 +0800

    feat(all): :sparkles: change file name

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,11 +1,12 @@
 const fs = require('fs')
 const path = require('path')
 const packages = fs.readdirSync(path.resolve(__dirname, 'packages'))
+const apps = fs.readdirSync(path.resolve(__dirname, 'apps'))
 
 module.exports = {
   extends:['@commitlint/config-conventional'],
   prompt: {
     useEmoji:true,
-    scopes: [...packages]
+    scopes: [...packages,...apps]
   }
 }
